bd175dc4db72aa0569c9b23f47885c3e31ac3f31
sasmitha007 <sasmithatp7@gmail.com>
2024-10-25 08:11:51 +0530

    Add files via upload

--- SequencedCollex.java
+++ SequencedCollex.java
@@ -1,21 +1,19 @@
 import java.util.LinkedList;
 import java.util.SequencedCollection;
 
-public class SequencedCollex{
-	public SequencedCollex()
-	{
-		SequencedCollection sc = new LinkedList();
-		sc.addFirst(7);
-		sc.addFirst(4);
-		sc.addFirst(5);
-		
-		System.out.println(sc.getFirst());
-		System.out.println(sc.removeLast());
-		System.out.println(sc);
-		System.out.println(sc.reversed());
-	}
-	
-	public static void main(String[] args){
-		new SequencedCollex();
-	}
+public class SequencedCollex {
+
+    public static void main(String[] args) {
+        // Create a LinkedList implementing SequencedCollection interface
+        SequencedCollection<Integer> sc = new LinkedList<>();
+        
+        sc.addFirst(7);
+        sc.addFirst(4);
+        sc.addFirst(5);
+        
+        System.out.println(sc.getFirst());
+        System.out.println(sc.removeLast());
+        System.out.println(sc);
+        System.out.println(sc.reversed());
+    }
 }
