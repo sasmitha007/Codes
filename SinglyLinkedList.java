4bf39e91462a6867304252eedf3a701128f48484
Sasmitha <sasmithatp7@gmail.com>
2025-02-15 23:00:09 +0530

    Update SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 public class SinglyLinkedList {
     private Node head; // Head of the list
 
