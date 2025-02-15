cb949304f4d30f20e6a939cdc998a1f30cf2f82b
Sasmitha <sasmithatp7@gmail.com>
2025-02-15 22:59:26 +0530

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 public class Node {
     public int data; // Value of the node
     public Node next; // Reference to the next node
