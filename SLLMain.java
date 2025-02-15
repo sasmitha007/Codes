bfdcb7a7b300ed081be3e05b6945f74c4fac7301
Sasmitha <sasmithatp7@gmail.com>
2025-02-15 22:59:54 +0530

    Update SLLMain.java

--- SLLMain.java
+++ SLLMain.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 public class SLLMain {
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList();
